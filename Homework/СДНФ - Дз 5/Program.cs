3d4a661bfde8ab2a546afcc116551a62e24fb173
nguyentienlong1106 <tienlong1106@gmail.com.vn>
2022-05-05 23:48:34 +0300

    этап 5+6

---
+++ Homework/СДНФ - Дз 5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Этап5___СДНФ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = "";
+            string[] symbols = new string[] { "a", "b", "c", "d", "e" };
+            WriteLine("Enter the value of k = ");
+            int k = int.Parse(ReadLine());
+            double m = Pow(2, k);
+            int n = (int)m;
+            int[,] table = new int[n, k + 1];
+            for (int i = 0; i < n; i++)
+            {
+                string[] x_y = ReadLine().Split(new[] { ' ' });
+                for (int j = 0; j <= k; j++)
+                {
+                    table[i, j] = int.Parse(x_y[j]);
+                }
+                x_y = null;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (table[i, k] == 1)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        if (table[i, j] == 1)
+                        {
+                            ans += symbols[j];
+                        }
+                        else
+                        {
+                            ans += "(-" + symbols[j] + ")";
+                        }
+                    }
+                    ans += "+";
+
+                }
+
+            }
+            WriteLine(ans.Remove(ans.Length-1, 1));
+        }
+    }
+}
+
---
+++ Homework/СКНФ - Дз 6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Этап6_СКНФ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans = "";
+            string[] symbols = new string[] { "a", "b", "c", "d", "e" };
+            WriteLine("Enter the value of k = ");
+            int k = int.Parse(ReadLine());
+            double m = Pow(2, k);
+            int n = (int)m;
+            int[,] table = new int[n, k + 1];
+            for (int i = 0; i < n; i++)
+            {
+                string[] x_y = ReadLine().Split(new[] { ' ' });
+                for (int j = 0; j <= k; j++)
+                {
+                    table[i, j] = int.Parse(x_y[j]);
+                }
+                x_y = null;
+            }
+            string ans1 = "";
+            for (int i = 0; i < n; i++)
+            {
+                
+                if (table[i, k] == 0)
+                {
+                    
+                    for (int j = 0; j < k; j++)
+                    {
+                        
+                        if (table[i, j] == 0)
+                        {
+                            ans1 += symbols[j];
+                        }
+                        else
+                        {
+                            ans1 += "-" + symbols[j];
+                        }
+                        ans1 += "+";
+                    }
+                    ans1 = ans1.Remove(ans1.Length - 1, 1);
+                    ans1 = "("+ans1+")";
+
+                }
+                ans += ans1;
+                ans1 = null;
+            }
+            WriteLine(ans);
+        }
+    }
+}
