f012530bbfd076f299ae7ea306ad54f9bd4dd2c3
AliceGuliaeva <you@example.com>
2022-05-10 13:06:49 +0300

    Этап 5,6

---
+++ Guliaeva_Alice/Lab2/СДНФ.py
@@ -0,0 +1,17 @@
+a=int(input())
+mas = []
+for i in range(2**a):
+    mas.append(list(map(int, input().split())))
+for i in range(2**a):
+    if mas[i][a]==1:
+        print("(", end="")
+        for k in range(a):
+           if mas[i][k]==0:
+              print('-',end="")
+           print(chr(ord('a')+k),end="")
+           if k<a-1:
+               print("^",end="")
+        if i==2**a-1:
+           print(")",end="")
+        else:
+            print(")+",end="")
